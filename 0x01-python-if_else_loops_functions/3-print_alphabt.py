6a4d66be012342f227680cf643d6682cd2d1980f
BRIANBKM <brianmaina900@gmail.com>
2022-09-20 15:22:39 +0000

    python

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/prothon3
+for letter in range(97, 123):
+	if chr(letter) != 'q' and chr(letter) = 'e':
+	print("{}", formart(chr(letter)), end="")
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for digit1 in range(0, 10):
+:	for digit2 in range(digit+1, 10):
+		print("{}{}".format(digit11, digit2))
+
