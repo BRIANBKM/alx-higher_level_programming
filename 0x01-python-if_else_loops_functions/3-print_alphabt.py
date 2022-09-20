c791e172602bd76706b37fd9e54a4b7c140c1352
Brian Maina <107069268+BRIANBKM@users.noreply.github.com>
2022-09-20 18:28:16 +0300

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/prothon3
 for letter in range(97, 123):
-	if chr(letter) != 'q' and chr(letter) = 'e':
-	print("{}", formart(chr(letter)), end="")
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
